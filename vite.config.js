aaaed40054fc59ce98dc223352deac03b8152160
cat-chicago <catalina.rivera.u@gmail.com>
2026-09-14 10:17:09 -0500

    Make GitHub Pages build self-contained

--- vite.config.js
+++ vite.config.js
@@ -3,10 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   base: '/Bridging-Craft/',
-  plugins: [react()],
-  build: {
-    rollupOptions: {
-      external: ['react', 'react-dom/client'],
-    },
-  },
+  plugins: [react({ jsxRuntime: 'classic' })],
 })
