fc259c510e02a96e9fb5c09f6ba30a9cacf80eac
cat-chicago <catalina.rivera.u@gmail.com>
2026-09-14 10:13:04 -0500

    Fix compact GitHub Pages build

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,9 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   base: '/Bridging-Craft/',
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      external: ['react', 'react-dom/client'],
+    },
+  },
 })
